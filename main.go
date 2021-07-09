b9c52ff62c8865b039556e366ec16a25d3c62e60
Barashkov-GO <gbarashkov2001@gmail.com>
2021-07-09 11:14:17 +0300

    fixes & tests added

--- main.go
+++ main.go
@@ -7,21 +7,41 @@ import (
 	"strings"
 )
 
+func deleteSideSpaces(inBytes []byte) (outBytes []byte) {
+	var (
+		indBeg int
+		indEnd int
+	)
+	indBeg = 0
+	for inBytes[indBeg] == ' ' || inBytes[indBeg] == '\t' || inBytes[indBeg] == '\n' || inBytes[indBeg] == '\r' {
+		indBeg += 1
+	}
 
-func entryPointCountExceed(a []byte) (bool, []byte){
-	s := string(a)
-	if strings.Count(s, "$ENTRY") != 1 {
-		return false, a
+	indEnd = len(inBytes) - 1
+	for inBytes[indEnd] == ' ' || inBytes[indEnd] == '\t' || inBytes[indEnd] == '\n' || inBytes[indBeg] == '\r' {
+		indEnd -= 1
 	}
-	ind := strings.Index(s, "$ENTRY")
-	if a[ind + 6] == ' ' && a[ind + 7] == 'G' && (a[ind + 8] == 'o' || a[ind + 8] == 'O') {
-		return true, a[strings.Index(string(a[ind + 8 :]), "{") + ind + 9 : strings.Index(string(a[ind + 8 :]), "}") + ind + 8]
-	}
-	return false, a
+	outBytes = inBytes[indBeg : indEnd]
+
+	return outBytes
 }
 
+// поиск точек входа и проверка на единственность
+func entryPointCountExceed(inBytes []byte) (bool, []byte){
+	s := string(inBytes)
+	if strings.Count(s, "$ENTRY") != 1 {
+		return false, inBytes
+	}
+	ind := strings.Index(s, "$ENTRY")
+	if inBytes[ind + 6] == ' ' && inBytes[ind + 7] == 'G' && (inBytes[ind + 8] == 'o' || inBytes[ind + 8] == 'O') {
+		return true, inBytes[strings.Index(string(inBytes[ind + 8 :]), "{") + ind + 9 : strings.Index(string(inBytes[ind + 8 :]), "}") + ind + 8]
+	}
+	return false, inBytes
+}
+
+// обработка файла
 func checkFile(path string) []byte {
-	a, err := ioutil.ReadFile(path)
+	inBytes, err := ioutil.ReadFile(path)
 	var (
 		indBeg int
 		indEnd int
@@ -34,40 +54,41 @@ func checkFile(path string) []byte {
 		log.Fatal(err)
 	}
 
-	for strings.Count(string(a), "/*") > 0 {
-		indBeg = strings.Index(string(a), "/*")
-		indEnd = strings.Index(string(a[indBeg+2:]), "*/") + indBeg + 2
-		a = append(a[:indBeg], a[indEnd+2:]...)
+	for strings.Count(string(inBytes), "/*") > 0 {	// удаление многострочных комментариев
+		indBeg = strings.Index(string(inBytes), "/*")
+		indEnd = strings.Index(string(inBytes[indBeg+2:]), "*/") + indBeg + 2
+		inBytes = append(inBytes[:indBeg], inBytes[indEnd+2:]...)
 	}
 
-	for strings.Count(string(a), "*") > 0 {
-		indBeg = strings.Index(string(a), "*")
-		indEnd1 = strings.Index(string(a[indBeg+1:]), "\n")
-		indEnd2 = strings.Index(string(a[indBeg+1:]), "*")
+	for strings.Count(string(inBytes), "*") > 0 {		// удаление однострочных комментариев
+		indBeg = strings.Index(string(inBytes), "*")
+		indEnd1 = strings.Index(string(inBytes[indBeg+1:]), "\n")
+		indEnd2 = strings.Index(string(inBytes[indBeg+1:]), "*")
 		if indEnd1 > indEnd2 {
 			indEnd = indEnd2 + indBeg + 1
 		} else {
 			indEnd = indEnd1 + indBeg + 1
 		}
-		a = append(a[:indBeg], a[indEnd+1:]...)
+		inBytes = append(inBytes[:indBeg], inBytes[indEnd+1:]...)
 	}
 
-	for strings.Count(string(a), "  ") > 0 {
-		indBeg = strings.Index(string(a), "  ")
-		a = append(a[:indBeg], a[indBeg + 1:]...)
+	for strings.Count(string(inBytes), "  ") > 0 {	// удаление двойных пробелов
+		indBeg = strings.Index(string(inBytes), "  ")
+		inBytes = append(inBytes[:indBeg], inBytes[indBeg + 1:]...)
 	}
 
-	flag, res := entryPointCountExceed(a)
-	if flag {
-		for _, v := range res {
-			fmt.Printf("%c", v)
+	notExceed, outBytes := entryPointCountExceed(inBytes)	// поиск точек входа
+	outBytes = deleteSideSpaces(outBytes)
+	if notExceed {
+		for _, outByte := range outBytes {
+			fmt.Printf("%c", outByte)
 		}
 	} else {
 		fmt.Println("Exceeding of entry points count")
 	}
-	return res
+	return outBytes
 }
 
 func main(){
-	checkFile("./tests/tests_auto/test_order.ref")
+	checkFile("./tests/test_....ref")	// подставьте вместо многоточия нужный файл проверки
 }
