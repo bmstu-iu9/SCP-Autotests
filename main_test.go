be6fa74c553b73d47492806e60b643f2311fac63
Maksim Kuzovchikov <mkuzovchikov@gmail.com>
2021-07-17 15:39:34 +0300

    Removed redundant strings.TrimSpace function call

--- main_test.go
+++ main_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"strings"
 	"testing"
 )
 
@@ -15,10 +14,8 @@ func TestCheckEntry(t *testing.T) {
 		t.Run(test.TestCase[0], func(t *testing.T) {
 			if res, err := checkFile(test.TestCase[0]); err != nil {
 				t.Error(err)
-			} else {
-				if test.Expected[0] != strings.TrimSpace(string(res)) {
-					t.Errorf("Incorrect result\nExpected: %s\nRecieved: %s", test.Expected[0], res)
-				}
+			} else if test.Expected[0] != string(res) {
+				t.Errorf("Incorrect result\nExpected: %s\nRecieved: %s", test.Expected[0], res)
 			}
 		})
 	}
